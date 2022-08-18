37873fb7cfcf4e471336f7d5c11e61c22167fac8
SrikarMK <srikarmks@gmail.com>
2022-08-18 20:34:25 +0530

    Create oops.py

---
+++ day_12/oops.py
@@ -0,0 +1,10 @@
+class animal:
+    def __init__(self,name,type):
+        self.name=name
+        self.type=type
+
+an=animal('Snoopy','dog')
+a1=animal('Boos','cat')
+print(an.name , a1.name)
+print(an.type)
+
