2b89e1442fc3e4dea352b6bd10398469de828ea4
SrikarMK <srikarmks@gmail.com>
2022-09-25 22:15:32 +0530

    Add files via upload

---
+++ day_50/pokeapi.py
@@ -0,0 +1,32 @@
+import requests
+
+while True:
+    pokeName=input("Enter the name of the pokemon\t").lower()
+
+    poke=requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokeName}')
+    data=poke.json()
+
+    for type in data['types']:
+        pokeType=type['type']['name']
+        print(f"{pokeName.upper()} is of {pokeType.upper()} type")
+
+
+    weight=data['weight']
+    print(f"{pokeName} weight is {weight} kg")
+
+    print('\t')
+    print(f"{pokeName.upper()}'s ATTACKS")
+    for ability in data['abilities']:
+        print(ability['ability']['name'])
+    print('\t')
+    print(f"{pokeName.upper()}'s STATS")
+    for stat in data['stats']:
+        baseStat=stat['base_stat']
+        statName=stat['stat']['name']
+        print(f'{statName} -> {baseStat}')
+
+    again=input('Do you want to select another pokemon? (Y/N) ').lower()
+    if 'y' in again:
+        continue
+    else:
+        break
