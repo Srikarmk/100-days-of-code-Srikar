7359d269f2a106d66bdef31c031bd1665fcb6413
SrikarMK <srikarmks@gmail.com>
2022-10-20 23:20:32 +0530

    Delete jsonfile.py

--- day_65/jsonfile.py
+++
@@ -1,9 +0,0 @@
-import json
-from textwrap import indent
-
-with open("P:/Python/Projects/names.json","r") as d:
-    data=json.load(d)
-    # data['like']="Movies"
-    # new_data=json.dump(data,indent=4)
-
-print(data)
