04409868fc2a5e1829df6ce951fd682ddc2d5220
SrikarMK <srikarmks@gmail.com>
2022-11-06 22:36:12 +0530

    Add files via upload

---
+++ day_73/madlibs.py
@@ -0,0 +1,21 @@
+#some ways to concatenate a string 
+'''
+name=input("What is your Name")
+
+print("Your name is "+ name)  //concatenation operator 
+print("your name is {}".format(name)) 
+print(f"your name is {name}") //f-string (formatted String)
+'''
+name=input("Name: ")
+place=input("Place: ")
+verb=input("Verb: ")
+funny=input("funny_name: ")
+belief=input("Your belief: ")
+actor=input("actor/actress name: ")
+show=input("Favourite Show: ")
+madlib=f"I am {name} , I live in {place} , i like to {verb}.My nickname is {funny}, Programming is {belief}.My favourite actor is {actor} and i love watching {show}."
+  
+print(madlib)
+
+# name=input()
+# print("your name is {}".format(name))
