b0a409901ba38cb96dbc61f061fda70b0aba2506
SrikarMK <srikarmks@gmail.com>
2022-11-06 22:37:18 +0530

    Add files via upload

---
+++ day_73/qrcode_gen.py
@@ -0,0 +1,8 @@
+import qrcode 
+import cv2
+qr=qrcode.make("https://github.com/Srikarmk")
+qr.save("github.jpg")
+
+img = cv2.QRCodeDetector()
+val,a,b= img.detectAndDecode(cv2.imread("github.jpg"))
+print(val) 
