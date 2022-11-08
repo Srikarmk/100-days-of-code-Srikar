aaa2f4c4b7ad9e1305d1439ef8dfea8ab5f82893
SrikarMK <srikarmks@gmail.com>
2022-11-08 20:05:32 +0530

    Delete first.py

--- day_73/first.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-
-#Reading imgs
-img = cv.imread('Photos/cat.jpg')  # returns a matrix of pixels
-
-cv.imshow('Cat',img)   #opens a window to display img
-
-cv.waitKey(0)   #waits for key press for a few milliseconds 0- infinite amount 
-
